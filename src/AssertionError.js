05eb0f03d7fb79adc8e99367a4d11a288d8f5bdb
Brian Cavalier <brian@hovercraftstudios.com>
2017-01-18 21:07:40 -0500

    feat(test): Add nyc code coverage
    AFFECTS: @briancavalier/assert

---
+++ src/AssertionError.js
@@ -0,0 +1,22 @@
+export class AssertionError extends Error {
+  constructor (message, stackTop) {
+    super(message)
+    this.message = message
+    captureStack(this, stackTop)
+  }
+}
+
+function captureStack (target, stackTop) {
+  /* istanbul ignore else */
+  if (Error.captureStackTrace) {
+    Error.captureStackTrace(target, stackTop)
+  } else {
+    const { stack } = new Error()
+    if (stack) {
+      const index = stack.indexOf(`\n${stackTop}`)
+      if (index >= 0) {
+        target.stack = stack.slice(stack.indexOf(`\n${index + 1}`) + 1)
+      }
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { curry2 } from '@most/prelude'
+import { AssertionError } from './AssertionError'
+
+export { AssertionError }
 
 export const assert = b => verify(assert, 'assertion failed', b)
 
@@ -11,22 +14,3 @@ const fail = (msg, stackTop) => {
   throw new AssertionError(msg, stackTop)
 }
 
-const captureStack = Error.captureStackTrace || tryToCaptureStack
-
-function tryToCaptureStack (target, stackTop) {
-  const { stack } = new Error()
-  if (stack) {
-    const index = stack.indexOf(`\n${stackTop}`)
-    if (index >= 0) {
-      target.stack = stack.slice(stack.indexOf(`\n${index + 1}`) + 1)
-    }
-  }
-}
-
-export class AssertionError extends Error {
-  constructor (message, stackTop) {
-    super(message)
-    this.message = message
-    captureStack(this, stackTop)
-  }
-}
